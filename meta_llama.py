9e783da141e7d6e7d8a63e7343d22ef73b4b6cfd
Choi Chan Hwan <africakokiri@gmail.com>
2024-12-07 14:54:31 +0900

    Meta Llama 추가
    - Meta의 AI인 Llama를 추가했습니다.

---
+++ meta_llama.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from llamaapi import LlamaAPI
+from dotenv import load_dotenv
+import os
+import json
+
+load_dotenv()
+
+
+def llama_prompt(prompt):
+    api_token = os.environ.get("LLAMA_API_KEY")
+
+    llama = LlamaAPI(api_token)
+
+    api_request_json = {
+        "model": "llama3.2-90b-vision",
+        "messages": [
+            {"role": "user", "content": prompt},
+        ],
+    }
+
+    # Make your request and handle the response
+    response = llama.run(api_request_json)
+    res = json.dumps(response.json(), indent=2)
+    res_to_json = json.loads(res)
+    content = res_to_json["choices"][0]["message"].get("content")
+
+    return content
