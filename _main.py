9f6425b6511880f4177233b7c5214ce1e2d1dd95
Choi Chan Hwan <africakokiri@gmail.com>
2024-12-07 16:25:29 +0900

    AI 채팅 영역 스타일링
    - AI들의 답변을 받는 컨테이너를 스타일링 했습니다.

--- _main.py
+++ _main.py
@@ -1,5 +1,6 @@
 import asyncio
 import streamlit as st
+import streamlit.components.v1 as components
 from openai_gpt import gpt_prompt
 from google_gemini import gemini_prompt
 from anthropic_claude import claude_prompt
@@ -12,22 +13,44 @@ st.set_page_config(layout="wide")
 prompt = st.chat_input("프롬프트를 입력하세요.")
 st.session_state["prompt"] = prompt
 
+st.markdown(
+    """
+        <style>
+            .stColumn {
+                background-color: #F1F2F6;
+                border-radius: 8px;
+                height: 510px;
+                padding: 16px;
+                max-width: 280px;
+            }
+            
+            .stChatMessage {
+                padding: 0px;
+                width: 100%;
+                display: flex;
+                justify-content: center;
+            }
+        </style>
+    """,
+    unsafe_allow_html=True,
+)
+
 
 # 비동기 API 호출 함수 정의
 async def fetch_gpt_response(prompt):
-    return gpt_prompt(prompt) if prompt else "gpt-4o-mini"
+    return gpt_prompt(prompt) if prompt else ""
 
 
 async def fetch_gemini_response(prompt):
-    return gemini_prompt(prompt) if prompt else "Gemini-1.5-flash"
+    return gemini_prompt(prompt) if prompt else ""
 
 
 async def fetch_claude_response(prompt):
-    return claude_prompt(prompt) if prompt else "Claude-3-5-sonnet"
+    return claude_prompt(prompt) if prompt else ""
 
 
 async def fetch_llama_response(prompt):
-    return llama_prompt(prompt) if prompt else "Llama3.2-90b-vision"
+    return llama_prompt(prompt) if prompt else ""
 
 
 # 비동기 처리 함수
@@ -45,8 +68,8 @@ async def fetch_all_responses(prompt):
 responses = asyncio.run(fetch_all_responses(prompt))
 
 # 탭 구성
-All, gpt_as_tab, gemini_as_tab, claude_as_tab, llama_as_tab = st.tabs(
-    ["전체", "chatGPT", "Gemini", "Claude", "llama"]
+All, gpt_as_tab, gemini_as_tab, claude_as_tab, llama_as_tab, settings = st.tabs(
+    ["전체", "chatGPT", "Gemini", "Claude", "llama", "설정"]
 )
 
 # 탭: 전체
@@ -56,22 +79,27 @@ with All:
     with gpt_as_col:
         with st.chat_message("ai", avatar="./assets/gpt.svg"):
             st.markdown("**openAI: gpt-4o-mini**")
-            st.write(responses[0])  # GPT 응답
+
+        st.write(responses[0])  # GPT 응답
 
     with gemini_as_col:
         with st.chat_message("ai", avatar="./assets/gemini.svg"):
             st.markdown("**Google: Gemini-1.5-flash**")
-            st.write(responses[1])  # Gemini 응답
+
+        st.write(responses[1])  # Gemini 응답
 
     with claude_as_col:
         with st.chat_message("ai", avatar="./assets/claude.svg"):
             st.markdown("**Anthropic: Claude-3-5-sonnet**")
-            st.write(responses[2])  # Claude 응답
+
+        st.write(responses[2])  # Claude 응답
 
     with llama_as_col:
         with st.chat_message("ai", avatar="./assets/meta.png"):
             st.markdown("**Meta: Llama3.2-90b-vision**")
-            st.write(responses[3])  # LLaMA 응답
+
+        st.write(responses[3])  # LLaMA 응답
+
 
 # 탭: chatGPT
 with gpt_as_tab:
@@ -96,3 +124,7 @@ with llama_as_tab:
     st.title("💬 Meta: Llama3.2-90b-vision")
     st.caption("🚀 A Streamlit chatbot powered by Meta LLaMA")
     st.write(responses[3])
+
+# 탭: 설정
+with settings:
+    st.title("💬 Meta: Llama3.2-90b-vision")
