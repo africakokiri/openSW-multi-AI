fc7ed2f60fefc37d0e8ce7d26382552ce23fb16f
Choi Chan Hwan <africakokiri@gmail.com>
2024-12-07 18:39:40 +0900

    Llama server 503으로 인한 API 교체
    - Llama 서버가 503 에러를 반환해 해결 방법이 없다고 판단하고 AI/ML API로 교체했습니다.

--- ai_ml.py
+++ ai_ml.py
@@ -1,26 +1,33 @@
 import requests
 import json
+from dotenv import load_dotenv
+import os
 
-response = requests.post(
-    url="https://api.aimlapi.com/chat/completions",
-    headers={
-        "Authorization": "Bearer 9e844f4c03124e4d86ab0d08be65de92",
-        "Content-Type": "application/json",
-    },
-    data=json.dumps(
-        {
-            "model": "gpt-4o",
-            "messages": [
-                {
-                    "role": "user",
-                    "content": "What kind of model are you?",
-                },
-            ],
-            "max_tokens": 512,
-            "stream": False,
-        }
-    ),
-)
+load_dotenv()
+
+
+def ai_api(model, prompt):
+    response = requests.post(
+        url="https://api.aimlapi.com/chat/completions",
+        headers={
+            "Authorization": "Bearer " + os.environ.get("AIML_API_KEY"),
+            "Content-Type": "application/json",
+        },
+        data=json.dumps(
+            {
+                "model": model,
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": prompt,
+                    },
+                ],
+                "max_tokens": 512,
+                "stream": False,
+            }
+        ),
+    )
+
 
 response.raise_for_status()
 print(response.json())
--- meta_llama.py
+++ meta_llama.py
@@ -1,25 +1,58 @@
 from llamaapi import LlamaAPI
 from dotenv import load_dotenv
+import requests
 import os
 import json
 
 load_dotenv()
 
 
+# def llama_prompt(prompt):
+#     api_token = os.environ.get("LLAMA_API_KEY")
+
+#     llama = LlamaAPI(api_token)
+
+#     api_request_json = {
+#         "model": "llama3.2-90b-vision",
+#         "messages": [
+#             {"role": "user", "content": prompt},
+#         ],
+#     }
+
+#     # Make your request and handle the response
+#     response = llama.run(api_request_json)
+#     res = json.dumps(response.json(), indent=2)
+#     res_to_json = json.loads(res)
+#     content = res_to_json["choices"][0]["message"].get("content")
+
+#     return content
+
+
+# print(llama_prompt("안녕"))
+
+
 def llama_prompt(prompt):
-    api_token = os.environ.get("LLAMA_API_KEY")
+    response = requests.post(
+        url="https://api.aimlapi.com/chat/completions",
+        headers={
+            "Authorization": "Bearer " + os.environ.get("AIML_API_KEY"),
+            "Content-Type": "application/json",
+        },
+        data=json.dumps(
+            {
+                "model": "meta-llama/Meta-Llama-3.1-405B-Instruct-Turbo",
+                "messages": [
+                    {
+                        "role": "user",
+                        "content": prompt,
+                    },
+                ],
+                "max_tokens": 512,
+                "stream": False,
+            }
+        ),
+    )
 
-    llama = LlamaAPI(api_token)
-
-    api_request_json = {
-        "model": "llama3.2-90b-vision",
-        "messages": [
-            {"role": "user", "content": prompt},
-        ],
-    }
-
-    # Make your request and handle the response
-    response = llama.run(api_request_json)
     res = json.dumps(response.json(), indent=2)
     res_to_json = json.loads(res)
     content = res_to_json["choices"][0]["message"].get("content")
